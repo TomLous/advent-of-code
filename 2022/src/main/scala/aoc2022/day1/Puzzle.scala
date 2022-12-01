e772da6309bdfb46d5bc5eb1f6b4af3b554b528a
Tom Lous <tomlous@gmail.com>
2022-12-01 22:20:48 +0100

    Restructured template 2022

--- 2022/src/main/scala/aoc2022/day1/Puzzle.scala
+++ 2022/src/main/scala/aoc2022/day1/Puzzle.scala
@@ -12,11 +12,14 @@ object Puzzle extends ZIOAppDefault {
     .flatMap(source => ZStream.fromIterator(source.getLines()))
 
   private val program = for {
-    data <- Solution.parseInput(puzzleData)
-    output1       <- Solution.solvePart1(data)
-    _             <- Console.printLine(s"Result of the puzzle in part 1: " + output1)
-    output2       <- Solution.solvePart2(data)
-    _             <- Console.printLine(s"Result of the puzzle in part 2: " + output2)
+    startTime <- zio.Clock.instant
+    data      <- Solution.parseInput(puzzleData)
+    output1   <- Solution.solvePart1(data)
+    _         <- Console.printLine(s"Result of the puzzle in part 1: " + output1)
+    output2   <- Solution.solvePart2(data)
+    _         <- Console.printLine(s"Result of the puzzle in part 2: " + output2)
+    endTime   <- zio.Clock.instant
+    _         <- Console.printLine(s"Time taken: " + (endTime.toEpochMilli - startTime.toEpochMilli) + "ms")
   } yield ()
 
   override def run: ZIO[Any, Any, Any] = program

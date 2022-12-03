e8a436ac74bb2c6263337ca2f383ab43201d0dd7
Tom Lous <tomlous@gmail.com>
2022-12-03 11:21:52 +0100

    Restructured day 9 a bit

--- 2021/src/main/scala/aoc2021/day9/Solution.scala
+++ 2021/src/main/scala/aoc2021/day9/Solution.scala
@@ -6,8 +6,8 @@ import zio.stream.*
 
 object Solution {
 
-  val findLowestPoint = ZPipeline.mapChunks[Chunk[List[Int]], (Int,Long)](_.flatMap(_.toList match
-    case previousLine :: currentLine :: nextLine :: Nil =>
+  val findLowestPoint = ZPipeline.mapChunks[Chunk[(List[Int], Long)], Point](_.flatMap(_.toList match
+    case (previousLine, _) :: (currentLine, y) :: (nextLine, _) :: Nil =>
       Chunk.fromIterator(
         (None +: currentLine.zipWithIndex.map(Some(_)) :+ None).sliding(3).flatMap{
           case previousItem :: Some(value, pos) :: nextItem :: Nil =>
@@ -15,7 +15,7 @@ object Solution {
             val down = nextLine.lift(pos)
             val left = previousItem.map(_._1)
             val right = nextItem.map(_._1)
-            if(List(up, down, left, right).flatten.forall(_ > value)) Some(value, pos.toLong) else None
+            if(List(up, down, left, right).flatten.forall(_ > value)) Some(Point(value, pos.toLong, y)) else None
         }
       )
     case other => throw new Exception(s"Unexpected list: $other")
@@ -26,13 +26,10 @@ object Solution {
   def parseInput(lineStream: ZStream[Any, Throwable, String]): ZIO[Any, Throwable, List[Input]] =
     (ZStream.fromChunk(Chunk(List.empty[Int])) ++ lineStream
       .map(parseLine) ++ ZStream.fromChunk(Chunk(List.empty[Int])))
+      .zipWithIndex
       .rechunk(1)
       .sliding(3)
       .via(findLowestPoint)
-      .zipWithIndex
-      .map{
-        case ((value, x), y) => LowPoint(value, x, y)
-      }
       .runCollect
       .map(_.toList)
 
--- 2021/src/main/scala/aoc2021/day9/model.scala
+++ 2021/src/main/scala/aoc2021/day9/model.scala
@@ -2,7 +2,7 @@ package aoc2021.day9
 
 object model {
 
-  type Input = LowPoint 
+  type Input = Point 
   
-  case class LowPoint(value: Int, x: Long, y: Long)
+  case class Point(value: Int, x: Long, y: Long)
 }

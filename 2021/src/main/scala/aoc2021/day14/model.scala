9ffa595df3c5c0f3a69ff4b2f752cce780d5921f
Tom Lous <tomlous@gmail.com>
2022-12-06 12:33:58 +0100

    Day 14 2021 solved with some hints :-)

--- 2021/src/main/scala/aoc2021/day14/model.scala
+++ 2021/src/main/scala/aoc2021/day14/model.scala
@@ -14,57 +14,35 @@ object model {
         .map(_.mkString)
         .toList
         .groupMapReduce(identity)(_ => 1L)(_ + _)
-       PolymerState(pairs, rules)
+      val counts = chars.groupMapReduce(identity)(_ => 1L)(_ + _)
+       PolymerState(pairs, counts, rules)
 
-  case class PolymerState(pairs: Map[String, Long], rules: Map[String, Char]):
-
-
+  case class PolymerState(pairs: Map[String, Long], charCount:Map[Char, Long], rules: Map[String, Char]):
 
     def score:Long =
-      val charSums = pairs.toList.flatMap{
-        case (pair, count) => pair.toCharArray.map((_, count))
-      }.groupMapReduce(_._1)(_._2)(_ + _).toList.sortBy(_._2)
-      println(charSums)
+      val charSums = charCount.toList.sortBy(_._2)
       charSums.last._2 - charSums.head._2
-
-
-
-    def iterate(n: Int) = (0 until n).foldLeft(this)((state, i) =>
-      println(s"iteration ${i+1}")
-      if(i == 0)
-        println("NCNBCHB".toCharArray.sliding(2).map(_.mkString).map(_ -> 1L).toList.groupMapReduce(_._1)(_._2)(_ + _).toList.sortBy(_._1).toMap)
-      if(i == 1)
-        println("NBCCNBBBCBHCB".toCharArray.sliding(2).map(_.mkString).map(_ -> 1L).toList.groupMapReduce(_._1)(_._2)(_ + _).toList.sortBy(_._1).toMap)
-      if(i == 2)
-        println("NBBBCNCCNBBNBNBBCHBHHBCHB".toCharArray.sliding(2).map(_.mkString).map(_ -> 1L).toList.groupMapReduce(_._1)(_._2)(_ + _).toList.sortBy(_._1).toMap)
-
-
-      state.iterateOnce
-    )
+    def iterate(n: Int): PolymerState = (0 until n).foldLeft(this)((state, i) => state.iterateOnce)
 
 
     def iterateOnce: PolymerState =
-      val x = pairs.toList.flatMap {
+      val (newPairsRaw, addSumsRaw) = pairs.toList.flatMap {
         case (pair, currentCount) =>
           rules.get(pair) match
             case Some(insertChar) =>
               List(
-                (s"${pair.head}$insertChar", currentCount),
-                (s"$insertChar${pair.last}", currentCount)
+                ((s"${pair.head}$insertChar", currentCount), Some((insertChar,currentCount))),
+                ((s"$insertChar${pair.last}", currentCount), None)
               )
-            case None => List((pair, currentCount))
-      }
+            case None =>
+              List(
+                ((pair, currentCount), None)
+              )
+      }.unzip
 
+      val newPairs = newPairsRaw.groupMapReduce(_._1)(_._2)(_ + _)
+      val newSums = (addSumsRaw.flatten ++ charCount.toList).groupMapReduce(_._1)(_._2)(_ + _)
 
-//      println(x.sortBy(_._1))
-//
-//
-      println(x.map(_._2).sum + 1)
-
-      val y = x.groupMapReduce(_._1)(_._2)(_ + _)
-
-      println(y)
-
-      PolymerState(y, rules)
+      PolymerState(newPairs, newSums, rules)
 
 }

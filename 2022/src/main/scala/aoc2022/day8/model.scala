861df1c9146e39f0fea6332363b0ec4fb4b938b5
Tom Lous <tomlous@gmail.com>
2022-12-08 10:59:12 +0100

    Day 8 2022 cleaned

--- 2022/src/main/scala/aoc2022/day8/model.scala
+++ 2022/src/main/scala/aoc2022/day8/model.scala
@@ -1,7 +1,7 @@
 package aoc2022.day8
 
 import breeze.generic.UFunc
-import breeze.linalg._
+import breeze.linalg.*
 
 object model {
 
@@ -9,48 +9,29 @@ object model {
 
   case class Grid(matrix: DenseMatrix[Int]):
 
-
-    // als tree < init tree
-    //    als tree >= vorige tree
-    // als tree >= inittree
-    //    +1 /stop
     lazy val optimalLocationScore: Long = {
       def visible(initHeight: Int)(seq: Seq[Int]) =
         seq
-          .foldLeft((0, 0, false)) {
-            case ((treeSeen, maxHeight, stopped), checkHeight) =>
-              if(stopped) (treeSeen, maxHeight, stopped)
-              else if (checkHeight < initHeight ) (treeSeen + 1, checkHeight, stopped)
-              else if (checkHeight >= initHeight) (treeSeen + 1, checkHeight, true)
-              else (treeSeen, maxHeight, stopped)
+          .foldLeft((0, false)) { case ((treeSeen, blocked), checkHeight) =>
+            if (blocked) (treeSeen, blocked)
+            else (treeSeen + 1, checkHeight >= initHeight)
           }
           ._1
 
-
-      def score(height: Int, directions:List[List[Int]]):Int =
-        val treesSeen = directions.map(visible(height))
-        println(directions)
-        println(height)
-        println(treesSeen)
-        println("score:" + treesSeen.product)
-        println("----")
-        treesSeen.product
-
+      def score(height: Int, directions: List[List[Int]]): Int =
+        directions.map(visible(height)).product
 
       (for {
         row <- 0 until matrix.rows
         col <- 0 until matrix.cols
-        leftVector = matrix(row, 0 until col).inner.toArray.toList
-        rightVector = matrix(row, col+1 until matrix.cols).inner.toArray.toList
-        upVector  = matrix(0 until row, col).toArray.toList
-        downVector = matrix(row+1 until matrix.rows, col).toArray.toList
-      } yield score(matrix(row, col), List(upVector.reverse, leftVector.reverse,rightVector, downVector))).max
+        leftVector  = matrix(row, 0 until col).inner.toArray.toList
+        rightVector = matrix(row, col + 1 until matrix.cols).inner.toArray.toList
+        upVector    = matrix(0 until row, col).toArray.toList
+        downVector  = matrix(row + 1 until matrix.rows, col).toArray.toList
+      } yield score(matrix(row, col), List(upVector.reverse, leftVector.reverse, rightVector, downVector))).max
     }
 
-
-
-
-    lazy val numVisibleFromOutside:Long ={
+    lazy val numVisibleFromOutside: Long =
 
       def visibleSet(seq: Seq[Tree]) =
         seq.tail
@@ -67,19 +48,14 @@ object model {
         row <- 0 until matrix.rows
         col <- 0 until matrix.cols
         tree = Tree(matrix(row, col), row, col)
-      } yield ((row, tree), (col, tree)))
-        .unzip match
+      } yield ((row, tree), (col, tree))).unzip match
         case (rowsRaw, colsRaw) => toList(rowsRaw) ++ toList(colsRaw)
-
 
       val bothSides = vectors ++ vectors.map(_.reverse)
 
       val visibleTrees = bothSides.flatMap(visibleSet).toSet
 
       visibleTrees.size
-    }
-
-
 
   object Grid:
     def apply(input: List[List[Int]]): Grid = Grid(DenseMatrix(input: _*))

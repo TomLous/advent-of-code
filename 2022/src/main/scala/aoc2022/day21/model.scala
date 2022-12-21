3a5fab2d384a3774dd3697cd6d955adf479f7d56
Tom Lous <tomlous@gmail.com>
2022-12-21 12:10:03 +0100

    Removec case class

--- 2022/src/main/scala/aoc2022/day21/model.scala
+++ 2022/src/main/scala/aoc2022/day21/model.scala
@@ -26,51 +26,51 @@ object model {
   case object X extends Op:
     def apply(x: BigInt): BigInt = x
 
-  object Op:
-    def findSolution(x: Result, y: Result): Result = (x, y) match
+  extension (opOrBigInt: Result)
+    def +(other: Result): Result  = opOrBigInt.action(other, add, subR, subR)
+    def -(other: Result): Result  = opOrBigInt.action(other, subL, add, subL)
+    def *(other: Result): Result  = opOrBigInt.action(other, mult, divR, divR)
+    def /(other: Result): Result  = opOrBigInt.action(other, divL, mult, divL)
+    def <>(other: Result): Result = opOrBigInt.findSolution(other)
+
+    private def add: Action  = x => y => x + y
+    private def mult: Action = x => y => x * y
+    private def subL: Action = x => y => x - y
+    private def subR: Action = x => y => y - x
+    private def divL: Action = x => y => x / y
+    private def divR: Action = x => y => y / x
+
+    private def action(other: Result, action: Action, reverseL: Action, reverseR: Action): Result = (opOrBigInt, other) match
+      case (x: Op, y: Op)         => x andThen y
+      case (x: BigInt, y: Op)     => y compose reverseR(x)
+      case (x: Op, y: BigInt)     => x compose reverseL(y)
+      case (x: BigInt, y: BigInt) => action(x)(y)
+
+    private def findSolution(other: Result): Result = (opOrBigInt, other) match
       case (x: BigInt, y: Op)               => y(x)
       case (x: Op, y: BigInt)               => x(y)
       case (x: BigInt, y: BigInt) if x == y => x
       case (x: Op, y: Op)                   => throw new Exception(s"No solution found $x = $y")
       case (x: BigInt, y: BigInt)           => throw new Exception(s"No solution found $x != $y")
 
-    def action(x: Result, y: Result, action: Action, reverseL: Action, reverseR: Action): Result = (x, y) match
-      case (x: Op, y: Op)         => x andThen y
-      case (x: BigInt, y: Op)     => y compose reverseR(x)
-      case (x: Op, y: BigInt)     => x compose reverseL(y)
-      case (x: BigInt, y: BigInt) => action(x)(y)
-
-  def add: Action  = x => y => x + y
-  def mult: Action = x => y => x * y
-  def subL: Action = x => y => x - y
-  def subR: Action = x => y => y - x
-  def divL: Action = x => y => x / y
-  def divR: Action = x => y => y / x
-
-  extension (opOrBigInt: Result)
-    def +(other: Result): Result  = Op.action(opOrBigInt, other, add, subR, subR)
-    def -(other: Result): Result  = Op.action(opOrBigInt, other, subL, add, subL)
-    def *(other: Result): Result  = Op.action(opOrBigInt, other, mult, divR, divR)
-    def /(other: Result): Result  = Op.action(opOrBigInt, other, divL, mult, divL)
-    def <>(other: Result): Result = Op.findSolution(opOrBigInt, other)
-
   case class MonkeyMath(input: List[Input]):
+    private val root = Monkey("root")
 
     private def rec(currentMonkey: Monkey): Result =
       input.find(_.monkey == currentMonkey) match
-        case Some(m) => m.eval(rec)
-        case None    => X //  this will be the unknown variable (humn)
+        case Some(in) => in.eval(rec)
+        case None     => X //  this will be the unknown variable (humn)
 
     lazy val monkeyValueRoot: BigInt =
-      rec(Monkey("root")) match
+      rec(root) match
         case b: BigInt => b
         case b: Op     => throw new Exception(s"No solution: $b")
 
     lazy val monkeyValueHuman: BigInt =
       val newInput = input.flatMap {
-        case MonkeyOp(Monkey("root"), a, b, _) => Some(MonkeyOp(Monkey("root"), a, b, '='))
-        case m if m.monkey.name == "humn"      => None
-        case i                                 => Some(i)
+        case MonkeyOp(`root`, a, b, _)    => Some(MonkeyOp(root, a, b, '='))
+        case m if m.monkey.name == "humn" => None
+        case m                            => Some(m)
       }
 
       MonkeyMath(newInput).monkeyValueRoot

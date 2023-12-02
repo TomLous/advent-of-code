663782ae232ca7c28e5da73de90e43cc2adf163f
Tom Lous <tomlous@gmail.com>
2023-12-02 09:13:14 +0100

    Updated template

--- 2023/src/main/scala/aoc2023/template/DayN.scala
+++ 2023/src/main/scala/aoc2023/template/DayN.scala
@@ -13,23 +13,24 @@ object DayN extends App:
   private def checkSolution(solution: Long, part: Int):Unit =
     val target = readLines(s"result-part$part").mkString.toLong
     assert(solution == target, s"$solution != $target -- Part $part failed!")
-    
-  //  
+
+  //
 
   private def parse(lines: Iterator[String]) = lines
 
-  
 
   private def solvePart1(inputFile: String):Long =
-    ???
+    val data  = parse(readLines(inputFile))
+    0L
 
   private def solvePart2(inputFile: String):Long =
-    ???
-    
+    val data  = parse(readLines(inputFile))
+    0L
+
   private lazy val part1Solution = solvePart1("input")
   private lazy val part2Solution = solvePart2("input")
 
-  
+
   checkExample("example-part1", solvePart1)
 //  println("part1: " + part1Solution)
 //  checkSolution(part1Solution, 1)
